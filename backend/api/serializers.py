9e437ce97b0fa208a02fe32c88431590863cec33
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 20:59:20 -0200

    [backend/serializers] Implement CreateUserSerializer
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Expense, UserExpense, UserCustom
+from django.contrib.auth import get_user_model
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
@@ -20,3 +21,22 @@ class UserCustomSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserCustom
         fields = '__all__'
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField(
+        write_only=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password', 'first_name', 'last_name')
+        write_only_fields = ('is_staff', 'is_superuser', 'is_active',)
+
+        def create(self, validated_data):
+            user = super(CreateUserSerializer, self).create(validated_data)
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
