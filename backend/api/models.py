e7fd33a65d67bb1c7870955a5de20a701520fc97
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 19:40:23 -0200

    [backend/models] Fix error on DateField in the Expense model
    Fix two errors in Expense model:
    
    	- "Name '_' is not defined": needed to import the idiomatic
    		method _
    	- missing datetime module import
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from decimal import Decimal
+import datetime
+from django.utils.translation import gettext as _
 
 
 class Expense(models.Model):
