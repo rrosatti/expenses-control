619ef9928ce355fdec34115decdb74d49fa667d0
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 20:38:56 -0200

    [backend] Initial implementation of serializers
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Expense, UserExpense, UserCustom
+from .serializers import ExpenseSerializer, UserExpenseSerializer,\
+    UserCustomSerializer
 
-# Create your views here.
+
+class ExpenseList(generics.ListCreateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+
+class ExpenseDetail(generics.RetrieveDestroyAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+
+class CreateExpense(generics.CreateAPIView):
+    serializer_class = ExpenseSerializer
+
+
+class UserCustomDetail(generics.RetrieveDestroyAPIView):
+    queryset = UserCustom.objects.all()
+    serializer_class = UserCustomSerializer
+
+
+class CreateUserCustom(generics.CreateAPIView):
+    serializer_class = UserCustomSerializer
+
+
+class UserExpenseList(generics.ListCreateAPIView):
+    queryset = UserExpense.objects.all()
+    serializer_class = UserExpenseSerializer
+
+
+class UserExpenseDetail(generics.RetrieveDestroyAPIView):
+    queryset = UserExpense.objects.all()
+    serializer_class = UserExpenseSerializer
