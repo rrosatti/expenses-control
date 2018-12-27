b271675840923f7f8e313de2628fe2771a2f0ae4
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-27 20:28:11 -0200

    [client] Initial implementation of ExpenseDetail screen
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

---
+++ expenses/src/components/ExpenseDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class ExpenseDetail extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Expense Detail Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default ExpenseDetail;
