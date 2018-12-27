2503e6c62451909e2924678d4039bcaf30a6cb08
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-27 20:26:36 -0200

    [client] Initial implementation for Expenses screen
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

---
+++ expenses/src/components/Expenses.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class Expenses extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Expenses Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default Expenses;
