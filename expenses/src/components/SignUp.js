90d1a99e7e20eba82a2fd8415b1f2a599112cbab
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-27 20:24:58 -0200

    [client] Initial implementation for SignUp screen
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

---
+++ expenses/src/components/SignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class SignUp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Sign Up Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default SignUp;
