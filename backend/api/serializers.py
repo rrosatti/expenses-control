27343efdb88ab80d42b0d17de72efa0006943b0a
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-29 20:15:09 -0200

    [backend] Fix silly error in CreateUserSerializer
    The create() func was under the Meta class, while it should be
    under the CreateUserSerializer class.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -47,8 +47,8 @@ class CreateUserSerializer(serializers.ModelSerializer):
         fields = ('username', 'password', 'first_name', 'last_name', 'email')
         write_only_fields = ('is_staff', 'is_superuser', 'is_active',)
 
-        def create(self, validated_data):
-            user = super(CreateUserSerializer, self).create(validated_data)
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
+    def create(self, validated_data):
+        user = super(CreateUserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
