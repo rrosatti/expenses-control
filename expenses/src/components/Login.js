bf3a023bd2e12811eebe3631e3c668e9871d7d66
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-29 13:48:57 -0200

    [client] Add more elements to Login Screen
    The TextInputs for username and password were added, as well as the
    Login and SignUp buttons. The API calls are not being made yet.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- expenses/src/components/Login.js
+++ expenses/src/components/Login.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  onUsernameChange(username) {
+    this.setState({username});
+  }
+
+  onPasswordChange(password) {
+    this.setState({password})
+  }
+
   render() {
     return (
       <View>
-        <Text>Login Screen</Text>
+        <TextInput
+          placeholder="username"
+          onChangeText={this.onUsernameChange}
+        />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={this.onPasswordChange}
+        />
+        <Button onPress={() => this.props.navigation.navigate('ExpensesScreen')} title='Login' />
+        <Button onPress={() => this.props.navigation.navigate('SignUpScreen')} title='SignUp' />
       </View>
     );
   }
