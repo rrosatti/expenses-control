5b6ba00d06d30648e089c435db17beb7c0f5a7ea
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-29 16:01:21 -0200

    [client] Add more elements to SignUp and make it works
    Some new elements for user creation were added to SignUp class,
    and the signup functionality is working.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- expenses/src/components/SignUp.js
+++ expenses/src/components/SignUp.js
@@ -1,12 +1,94 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
 
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: '',
+      last_name: '',
+      username: '',
+      password: '',
+      email: '',
+    }
+    this.onFirstNameChange = this.onFirstNameChange.bind(this);
+    this.onLastNameChange = this.onLastNameChange.bind(this);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  onFirstNameChange(first_name) {
+    this.setState({first_name})
+  }
+
+  onLastNameChange(last_name) {
+    this.setState({last_name})
+  }
+
+  onEmailChange(email) {
+    this.setState({email})
+  }
+
+  onUsernameChange(username) {
+    this.setState({username})
+  }
+
+  onPasswordChange(password) {
+    this.setState({password})
+  }
+
+  handleSignUp() {
+    endpoint = '/auth/register/';
+    payload = {
+      username: this.state.username,
+      password: this.state.password,
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email
+    };
+
+    axios
+      .post(endpoint, payload)
+      .then(response => {
+        const { token, user } = response.data;
+
+        // set the returned token as the default auth header
+        axios.defaults.headers.common.Authorization = `Token ${token}`;
+
+        this.props.navigation.navigate('LoginScreen');
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <View>
-        <Text>Sign Up Screen</Text>
+        <TextInput
+          placeholder="first name"
+          onChangeText={this.onFirstNameChange}
+        />
+        <TextInput
+          placeholder="last name"
+          onChangeText={this.onLastNameChange}
+        />
+        <TextInput
+          placeholder="email"
+          onChangeText={this.onEmailChange}
+        />
+        <TextInput
+          placeholder="username"
+          onChangeText={this.onUsernameChange}
+        />
+        <TextInput
+          placeholder="password"
+          onChangeText={this.onPasswordChange}
+          secureTextEntry={true}
+        />
+        <Button onPress={this.handleSignUp} title="OK" />
       </View>
     );
   }
