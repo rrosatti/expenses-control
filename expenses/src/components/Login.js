9e6881e89a1159ca85a892c9a4bae0eca972c4f6
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-29 14:54:34 -0200

    [client] Implement login functionality
    If login succeeds, we save the returned token as the default auth
    header. It's necessary to make other API calls in the future.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- expenses/src/components/Login.js
+++ expenses/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button, TextInput } from 'react-native';
+import axios from 'axios';
 
 
 class Login extends Component {
@@ -11,6 +12,7 @@ class Login extends Component {
     };
     this.onUsernameChange = this.onUsernameChange.bind(this);
     this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   onUsernameChange(username) {
@@ -19,6 +21,23 @@ class Login extends Component {
 
   onPasswordChange(password) {
     this.setState({password})
+  }
+
+  handleLogin() {
+    const endpoint = '/auth/login/';
+    const payload = { username: this.state.username, password: this.state.password }
+
+    axios
+      .post(endpoint, payload)
+      .then(response => {
+        const { token, user } = response.data;
+
+        // set the returned token as the default auth header
+        axios.defaults.headers.common.Authorization = `Token ${token}`;
+
+        this.props.navigation.navigate('ExpensesScreen');
+      })
+      .catch(error => console.log(error))
   }
 
   render() {
@@ -33,7 +52,7 @@ class Login extends Component {
           secureTextEntry={true}
           onChangeText={this.onPasswordChange}
         />
-        <Button onPress={() => this.props.navigation.navigate('ExpensesScreen')} title='Login' />
+        <Button onPress={this.handleLogin} title='Login' />
         <Button onPress={() => this.props.navigation.navigate('SignUpScreen')} title='SignUp' />
       </View>
     );
