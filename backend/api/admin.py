3c19e42b3a3a5304e2bfd59a5da54b7d27553e73
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-29 21:06:44 -0200

    [backend/admin] Add custom Models in the admin interface
    With this patch, we can show the information in a better way in the
    admin interface (backoffice).
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Expense, UserCustom
 
-# Register your models here.
+
+class ExpenseAdmin(admin.ModelAdmin):
+    model = Expense
+
+    list_display = (
+        'id', 'value', 'date', 'short_desc',
+        'long_desc', 'get_user')
+
+    verbose_name = "Expenses List"
+
+    def get_user(self, obj):
+        return obj.user.username
+
+    get_user.short_description = 'User'
+
+
+class UserCustomAdmin(admin.ModelAdmin):
+    model = UserCustom
+
+    list_display = ('get_user', 'max_value', 'send_notifications')
+
+    verbose_name = 'Users Expense List'
+
+    def get_user(self, obj):
+        return obj.user.username
+
+    get_user.short_description = 'User'
+
+
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(UserCustom, UserCustomAdmin)
