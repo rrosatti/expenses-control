6aaf6d9b39b78dcd501d0a9288c13e05169f1e24
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-29 13:03:52 -0200

    [client] Start using react-navigation
    Initial implementation to use react-navigation.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- expenses/App.js
+++ expenses/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/components/Login';
+import SignUp from './src/components/SignUp';
+import Expenses from './src/components/Expenses';
+import ExpenseDetail from './src/components/ExpenseDetail';
+import NewExpense from './src/components/NewExpense';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+
+const AppNavigator = createStackNavigator(
+  {
+    LoginScreen: { screen: Login },
+    SignUpScreen: { screen: SignUp },
+    ExpensesScreen: { screen: Expenses },
+    ExpenseDetailScreen: { screen: ExpenseDetail },
+    NewExpenseScreen: { screen: NewExpense }
+  },
+  {
+    initialRouteName: 'LoginScreen'
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
